e9b2f25596f31115dab6505e7aebe867119c9868
sohale <Sohail.Siadatnejad@lloydsbanking.com>
2019-08-16 15:19:11 +0100

    join operator

--- templates/template-works.js
+++ templates/template-works.js
@@ -1,18 +1,28 @@
 /* RegExp operations + RegExps marketplace. A simple RegExps language or myteampltes? Also for building a good RegExp for work */
 
 function group(re) {
+    // only if necessary
     return new RegExp('(' + re.source + ')');
 }
-
+function plus_able(...args) {
+    return !"if concatable without parentheses";
+} 
 /*
 function concat(re1, re2) {
     return new RegExp('(' + re1.source + re2.source + ')');
 }
 */
 function concat(...re_arr) {
-    return group( re_arr.map(re => re.source).join('|') );
+    console.log('concat:  ', re_arr);
+    return group( new RegExp( re_arr.map(re => re.source).join('|') ));
 }
-
+function join(repeated, separator) {
+    return concat( group( new RegExp(concat( repeated, separator ).source + '*')), repeated );
+}
+/** strip_extra_parantheses */
+function simplify(re) {
+    return re;
+}
 
 const domainname_with_dot = /((?!-)[A-Za-z0-9-]{1,63}(?<!-)\.)+[A-Za-z]{2,6}/;
 const domainname = /((?!-)[A-Za-z0-9-\.]{1,69}(?<!-))/;
@@ -24,7 +34,7 @@ const httphttps = /http(s?):\/\//;
 const foldername = /([A-Za-z0-9-\.]+)/;
 // const path = concat(group( foldername.source + /\//.source ));
 // const path = concat(plus(foldername, + /\// )));
-// const path = concat(plus(foldername, + /\// )));
+const path = join(foldername, /\// );
 
 
 const testcases = [
@@ -43,7 +53,8 @@ const testcases = [
 
 
 testcases.forEach( url => {
-  const full_url = new RegExp(httphttps.source + domainname.source);
+  const full_url = concat(httphttps, domainname, path );
+  console.log(full_url.source)
 
   console.log(
 
