05878157b2f44c786759b1543f9b136ad9a1540f
sohale <Sohail.Siadatnejad@lloydsbanking.com>
2019-09-13 10:10:17 +0100

    refactoring all source/filenames

--- flow-obidc/oidc/flow-oidc-hybrid-complete.js
+++ flow-obidc/oidc/flow-oidc-hybrid-complete.js
@@ -1,76 +1,57 @@
 'use strict';
 
-// const fabrics = require('./fabrics-shared.js');
-const {Schema_from_swagger, require_yaml} = require('./templator/swagger2-schema.js');
-
-const {from_file} = require('./templator/from_file.js');
-// console.log( new from_file('./jws/1_public.key') );
-//const {all_non_undefined}  = require('./error-checking.js');
+const {Schema_from_swagger, require_yaml} = require('../templator/swagger2-schema.js');
+const {check_format_keys} = require('../fabrics-shared.js');
+const {from_file} = require('../templator/from_file.js');
 
 // components
-const {component_jws_verifysignature, accesstoken_from_gktvo} = require('./jwt_tools.js');
-const {call_post_style_1, style_3_call__POST_bearer_matls, call_get_style1} = require('./rest.js');
+const {component_jws_verifysignature, accesstoken_from_gktvo} = require('../jwt_tools.js');
+const {call_post_style_1, style_3_call__POST_bearer_matls, call_get_style1} = require('../rest.js');
 
-const {check_format_keys} = require('./fabrics-shared.js');
 
 function stage(stage_id, minor_step, heading) {
     console.log('---------- stage %d.', stage_id, minor_step, ':', heading);
 }
 
-class B64Url {
+const SOURCES = {
+    KEYS: {
+        TLS: {
+            tokenEP: {
+                // require
+                token_tls_certs: '../sensitive-data/SIT01-OBIE/cached-data/ma-tls-1.js',
+            },
+            aisp1: {
+                matls_key_filename: '../sensitive-data/SIT01-OBIE/cached-data/aka_transport.key',
+                matls_cert_filename: '../sensitive-data/SIT01-OBIE/cached-data/aka_transport.pem',
+            },
+        },
+        ssa2pubky_pemfile: '../sensitive-data/SIT01-OBIE/cached-data/ssa-jws-pubkey.pem',
+    },
+    // always require() only:
+    COMPILED_DATA: {
+        company_config: '../company-config.js',
+    },
 }
 
-class BinaryBuffer {
-    // from/to string utf-8
+const FILES = {
+    formats: {
+        // require_yaml
+        token_schema: '../token1.schema.yaml',
+        wellknown_schema: '../wellknown.swagger.yaml',
+    },
 }
-
-const {util: {base64url_decode_to_binary}} = require('./templator/b64url.js');
-
-
 
 const part2 = async (token_endpoint, {clientId,clientSecret}, body_data) =>{
     stage(2,1, 'calling the `/token` - using clientId');
 
-    // me:
-    // necesary objects. not more. (extension of no-move/no-cloning)
-
-    // bottleneck:
-    //     (token_endpoint, {clientId, clientSecret}, body_data = "grant_type=client_credentials&scope=openid accounts";)
-
-    /*
-        token_endpoint
-        (clientId, clientSecret),
-        querystring.parse('grant_type=client_credentials&scope=openid accounts')
-        querystring.stringify({ grant_type: 'client_credentials', scope: 'openid accounts' })
-    */
-
-    // then: prapare:
-    // const: (source): given from outise: from company_config
-    //      company_config.app is creatd by "App Creation" tasks.
-    //          SSA:
-    //               Note that they have already provided SSA (in the larger circuit).
-    //               SSA has two purposes: 1. certify (Also to arrive to use THROUGH a different ROUTE: form OB). 2. contains info fior us. 
-
-    // // scopes, grant (and flow) type.
-    // const body_data =
-
-
-    const key_cert_tuple = require(
-        './sensitive-data/SIT01-OBIE/cached-data/ma-tls-1.js',
-    );
-    // console.log('key_cert_tuple***', key_cert_tuple);
+    const key_cert_tuple = require(SOURCES.KEYS.TLS.tokenEP.token_tls_certs);
 
     const b = await call_post_style_1(token_endpoint, {clientId,clientSecret}, body_data, key_cert_tuple);
 
     const bobj = JSON.parse(b);
-    const checker = new Schema_from_swagger(require_yaml('./token1.schema.yaml'));
+    const checker = new Schema_from_swagger(require_yaml(FILES.formats.token_schema));
     checker.resolve(bobj); // throws if wrong
     return bobj;
-
-    //} catch(non200) {
-    //    console.log('1. none-200 status:', non200);
-    //    process.exit(1);
-    //}
 };
 
 
@@ -127,12 +108,9 @@ async function part5(uri, access_token /*, SOURCES*/) {
     );
     */
 
-    const SOURCES = {
-        matls_key_filename: './sensitive-data/SIT01-OBIE/cached-data/aka_transport.key',
-        matls_cert_filename: './sensitive-data/SIT01-OBIE/cached-data/aka_transport.pem',
-    };
-    const matls_key = new from_file(SOURCES.matls_key_filename).generate(null).toString();
-    const matls_cert = new from_file(SOURCES.matls_cert_filename).generate(null).toString();
+
+    const matls_key = new from_file(SOURCES.KEYS.TLS.aisp1.matls_key_filename).generate(null).toString();
+    const matls_cert = new from_file(SOURCES.KEYS.TLS.aisp1.matls_cert_filename).generate(null).toString();
     const key_cert_tuple = {key: matls_key, cert: matls_cert};
 
     const b = await style_3_call__POST_bearer_matls({
@@ -154,14 +132,16 @@ async function doit() {
     try {
         stage(1,1, 'wellknown point - taken from config');
         // todo: use "npm comment-json"
-        const company_config = require('./company-config.js');
+        const company_config = require(SOURCES.COMPILED_DATA.company_config );
         console.log(company_config.wellknown);
+
+        console.log(company_config);
+
 
         stage(1,2, 'calling the wellknown point');
         const a = await call_get_style1(company_config.wellknown);
         const jsonated = JSON.parse(a);
-        const checker = new Schema_from_swagger(require_yaml('./wellknown.swagger.yaml'));
-        //checker.resolve(a);
+        const checker = new Schema_from_swagger(require_yaml(FILES.formats.wellknown_schema));
         checker.resolve(jsonated); // throws if wrong
 
         stage(1,4, 'finding the `/token` endpoint - from contents from wellknown');
@@ -182,10 +162,6 @@ async function doit() {
         );
 
 
-        // {token_type, access_token,expires_in,consented_on,scope,openid accounts} = 
-        // fully verify:
-        // {token_type: 'Bearer'}
-        // {"scope":"openid accounts"}
         const access_toekn_gktvo = tokencall1_resp.access_token;
         console.log('access_toekn_gktvo:', access_toekn_gktvo);
         const access_token__jws_string =  accesstoken_from_gktvo(access_toekn_gktvo);
@@ -193,22 +169,12 @@ async function doit() {
         {
             const access_token__jws_string__reproduced = component_jws_verifysignature(
                 access_token__jws_string,
-                //'./sensitive-data/sit01-obwac/luat01-token.key????'
-                // ./sensitive-data/SIT02-OBIE/cached-data/...
-                {key_filename: './sensitive-data/SIT01-OBIE/cached-data/ssa-jws-pubkey.pem'}
+                {key_filename: SOURCES.KEYS.ssa2pubky_pemfile}
             );
             console.log({access_token__jws_string__reproduced});
-            // Why a JWS is sent to the client?
-            //     by the token endpoint (first call)
-            // The client needs to be able to validate it?
-            //
         }
 
-        // How to make sure the names are chosen correctly
         const access_token = access_token__jws_string;
-
-        // Where does SSA stand?
-
 
         const account_access_consents_url = company_config["account-access-consents"]({obver: 'v3.1'});
         part5(account_access_consents_url, access_token);
