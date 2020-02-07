8a688a6f769e3e0c442cb71baacd4084e3a63a75
Sohail Siadatnejad <sohale@gmail.com>
2020-02-07 21:26:39 +0000

    hoomogenising test2. It fails of course.

--- templators/requiret.js
+++ templators/requiret.js
@@ -1,13 +1,17 @@
 'use strict';
 
+const util = require('util');
 const path = require('path');
 
 const TEMPLATORS_FOLDER = 'templator';
 
 function requiret(templator_name) {
-    const command = `requiret('${templator_name}')`; 
+    const command = `requiret('${templator_name}')`;
     if (/\.js$/.exec(templator_name)) {
         throw new Error('dont use .js in ' + command);
+    }
+    if (!util.isString(templator_name)) {
+        throw new Error('Templator must be a string: ' + typeof(templator_name)  + ' ' + templator_name);
     }
     const fullpath = path.join(__dirname, TEMPLATORS_FOLDER, templator_name + '.js');
     return require(fullpath);
--- templators/templator/simple-demos/example-aspiration.js
+++ templators/templator/simple-demos/example-aspiration.js
@@ -2,12 +2,33 @@
 'use strict';
 
 // untested psudo-code
-
+/*
+TODO:
 const trequire = require('templator-require');
 
+OR: (not good)
+const templator = require('templator-require');
+const b64 = teamplator('base64');
+
+*/
+
+const {requiret} = require('../../requiret');
+const trequire = requiret;
+
+global.templatorsconf =  {
+  reverberate: false,
+}
+
+/*
+TODO: usage:
 const b64 = trequire('base64');
 const t = b64();
-t.generate();
+*/
+const {Base64} = trequire('base64');
+const b64 = Base64;
+const t = new b64();
+
+t.generate('sample string');
 
 const auto_test_templators = [
   {
@@ -19,8 +40,10 @@ const auto_test_templators = [
 function each_case(tname) {
   global.templatorsconf.reverberate = false;
 
+  console.log('going for: ', tname);
+
   const t = trequire(tname);
-  const instance = t();
+  const instance = new t();
   const ex1_trequire_example_geenrator = trequire_examples(tname);
   while (true) {
     const {example, expected} = ex1_trequire_example_geenrator.get_next_pair(); // make it more yeild-y. Or async.
@@ -37,5 +60,7 @@ function each_case(tname) {
   }
   // check documentations, etc
 }
+
+auto_test_templators.forEach( tname => each_case(tname.name) );
 
 // todo: commandline for each templator. testt base64;testt b64url;
