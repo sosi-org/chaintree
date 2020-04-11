ebdf764dd693a83d119ec8cd1fcfa5eb0ec93354
Sohail Siadatnejad <sohale@gmail.com>
2020-04-11 12:35:55 +0100

    adding git-missing file b64url.examples.js back from 12 Feb 16:00

---
+++ templators/templator/b64url.examples.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+function* example_generator() {
+  yield {output: null, input: Buffer.from('abc'), params: ['./templators/tests/simple-test-file.txt'] };
+}
+
+module.exports = example_generator;
